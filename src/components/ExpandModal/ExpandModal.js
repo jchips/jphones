0d82ce9254794aff7404829f72f6ea2c55a62143
Jchips <20979096+Jchips@users.noreply.github.com>
2024-03-05 23:32:24 -0800

    centered modal on mobile devices

--- src/components/ExpandModal/ExpandModal.js
+++ src/components/ExpandModal/ExpandModal.js
@@ -3,10 +3,26 @@ import { Modal } from 'react-bootstrap';
 import './ExpandModal.scss';
 
 class ExpandModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile: false
+    }
+  }
+  componentDidMount() {
+    const handleResize = () => {
+      this.setState({ isMobile: window.innerWidth <= 640 });
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }
   render() {
     const { expandData, bubble, handleCloseExpand, showExpandModal } = this.props;
     return (
-      <Modal show={showExpandModal} onHide={handleCloseExpand}>
+      <Modal show={showExpandModal} onHide={handleCloseExpand} centered={this.state.isMobile}>
         <Modal.Header closeButton>
           <Modal.Title>{bubble}</Modal.Title>
         </Modal.Header>
